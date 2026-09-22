91e7bca8b2e1606b5fe95bda507a9e884fa0bd5d
jblessing2109 <jenn.s.blessing@gmail.com>
2026-09-22 15:50:58 -0400

    Update timeline by removing 'get_location'
    Removed 'get_location' from the timeline sequence.

--- squared/squared.js
+++ squared/squared.js
@@ -1152,4 +1152,4 @@ var preload = {
 	images: [al, ar, ml_fr, mr_fl, rarr, larr]
 }
 
-timeline.push(preload, get_participant_id, get_location, welcome, enter_fullscreen, flanker_task, conclusion, exit_fullscreen);
+timeline.push(preload, get_participant_id, welcome, enter_fullscreen, flanker_task, conclusion, exit_fullscreen);
