75abc987b6846601619535a8bbff54b0772425d2
jblessing2109 <jenn.s.blessing@gmail.com>
2026-09-23 12:23:11 -0400

    Simplify conclusion stimulus for task completion

--- squared/squared.js
+++ squared/squared.js
@@ -1130,12 +1130,9 @@ var simon_task = {timeline: [intro_simon, threetwoone, block_simon_practice, pre
 // Ending screen
 var conclusion = {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: function() { return '<p style="font-size:25px;"> You earned the following points in the three tasks: <br>' +
-	          '<p> Colors Task: ' + total_stroop + ' points</p>' +
-			  '<p> Multiple Arrows Task: ' + total_flanker + ' points</p>' +
-			  '<p> Single Arrow Task: ' + total_simon + ' points</p>' +
-			  '<p style="font-size:25px;">You are now finished with this set of tasks.</p>' +
-              '<p style="font-size:25px;"><b> Press any key to exit.</b></p>' }
+   stimulus: function() { return '<p style="font-size:25px;">You earned ' + total_flanker + ' points on the Multiple Arrows Task.</p>' +
+    '<p style="font-size:25px;">You are now finished with this task.</p>' +
+    '<p style="font-size:25px;"><b>Press any key to exit.</b></p>' }
 }
 
 var exit_fullscreen = {
