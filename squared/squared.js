6205ef1d0279cf2e6d67be1cee3a2b8156997491
jblessing2109 <jenn.s.blessing@gmail.com>
2026-09-25 14:23:39 -0400

    Reduce practice and main task durations

--- squared/squared.js
+++ squared/squared.js
@@ -65,8 +65,8 @@ setTimeout(function() {
 // jsPsych.randomization.setSeed('squaredtasks');
 
 // IMPORTANT: THESE ARE THE ONLY VARIABLES THAT SHOULD BE MANUALLY CHANGED (if at all), the rest should be adaptive
-var practice_duration = 30000; // duration of practice
-var main_duration = 90000; // duration of main task
+var practice_duration = 3000; // duration of practice
+var main_duration = 5000; // duration of main task
 
 //general variables for use throughout the experiment
 var online = 1; // Numeric: 0 indicating that task is being run locally, 1 indicating that the task is being run through a platform
