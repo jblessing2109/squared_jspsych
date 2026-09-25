1276f9a09d4549e162aff17f27ec03d534d8e647
jblessing2109 <jenn.s.blessing@gmail.com>
2026-09-25 14:37:35 -0400

    Update timeline by removing exit_fullscreen
    Removed exit_fullscreen from the timeline.

--- squared/squared.js
+++ squared/squared.js
@@ -1180,5 +1180,5 @@ var preload = {
 	images: [al, ar, ml_fr, mr_fl, rarr, larr]
 }
 
-timeline.push(get_participant_id, welcome, enter_fullscreen, flanker_task, conclusion, exit_fullscreen);
+timeline.push(get_participant_id, welcome, enter_fullscreen, flanker_task, conclusion);
 jsPsych.run(timeline); 
