38351f9532f5a2d740ac7a580b2f7a850be17f13
jblessing2109 <jenn.s.blessing@gmail.com>
2026-09-25 15:01:52 -0400

    Add exit_fullscreen to timeline

--- squared/squared.js
+++ squared/squared.js
@@ -1180,5 +1180,5 @@ var preload = {
 	images: [al, ar, ml_fr, mr_fl, rarr, larr]
 }
 
-timeline.push(get_participant_id, welcome, enter_fullscreen, flanker_task, conclusion);
+timeline.push(get_participant_id, welcome, enter_fullscreen, flanker_task, conclusion, exit_fullscreen);
 jsPsych.run(timeline); 
